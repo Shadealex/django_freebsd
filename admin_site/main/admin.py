5eebf1385c8fa3abb9a62e76ea08623b65d0ee2d
Shadealex <5hnm@ukr.net>
2016-03-31 12:06:54 +0300

    Добавил раширеия джаниги, поправил игнор, мучал модели.......безуспешно.

--- admin_site/main/admin.py
+++ admin_site/main/admin.py
@@ -1,3 +1,18 @@
+#-*-coding: utf-8 -*-
 from django.contrib import admin
+from main.models import Verify_type, Server
 
 # Register your models here.
+class Verify_typeAdmin(admin.ModelAdmin):
+	list_display = ['name', 'visibility']
+	list_filter = ['visibility']
+	search_fields = ['name']
+
+admin.site.register(Verify_type, Verify_typeAdmin)
+
+class ServerAdmin(admin.ModelAdmin):
+	list_display = ['name', 'ip', 'date_add', 'date_edit', 'on_off', 'comment']
+	list_filter = ['name', 'ip', 'verify', 'on_off']
+	search_fields = ['name', 'ip']
+
+admin.site.register(Server, ServerAdmin)
--- admin_site/main/models.py
+++ admin_site/main/models.py
@@ -1,25 +1,9 @@
+#-*-coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 # Create your models here.
-class Server(models.Model):
-	name = models.TextField(_('Имя сервера'))
-	ip = models.GenericIPAddressField(protocol='both', related_name=_('IP сервера'))
-	date_add = models.DateField(auto_now_add=False, related_name=_('Дата добавления'))
-	date_edit = models.DateField(auto_now=False, related_name=_('Дата редактирования'))
-	verify = models.ManyToManyField(Verify_type, related_name='servers')
-	on_off = models.BooleanField(related_name=_('Включение'))
-	comment = models.TextField(related_name=_('Комментарий'))
-
-	def __unicode__(self):
-		return self.name
-
-	class Meta:
-		ordering = ['order_num']
-		db_table = 'servers'
-		verbose_name = _("Сервер")
-		verbose_name_plural = _("Сервера")
 
 # class Statistic(models.Model):
 	# pass
@@ -28,14 +12,32 @@ class Server(models.Model):
 		# return self.name
 
 class Verify_type(models.Model):
-	name = models.CharField(related_name=_('Название'), max_length=255)
-	visibility = models.BooleanField(related_name=_('Статус'), default=True, db_index=True)
+	name = models.CharField(verbose_name = _('Название'), max_length=255)
+	visibility = models.BooleanField(verbose_name = _('Статус'), default=True, db_index=True)
 
 	def __unicode__(self):
 		return self.name
 
 	class Meta:
-		ordering = ['order_num']
+		ordering = ['name']
 		db_table = 'verify_type'
-		verbose_name = _("Проверка")
-		verbose_name_plural = _("Проверки")
+		verbose_name = _("Тип проверки")
+		verbose_name_plural = _("Типы проверок")
+
+class Server(models.Model):
+	name = models.TextField(verbose_name = _('Имя сервера'))
+	ip = models.GenericIPAddressField(protocol='both', verbose_name = _('IP сервера'))
+	date_add = models.DateField(auto_now_add=False, verbose_name = _('Дата добавления'))
+	date_edit = models.DateField(auto_now=False, verbose_name = _('Дата редактирования'))
+	verify = models.ManyToManyField(Verify_type, related_name='servers', verbose_name = _('Проверки'), blank=True)
+	on_off = models.BooleanField(verbose_name = _('Включение'))
+	comment = models.TextField(verbose_name = _('Комментарий'))
+
+	def __unicode__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['name']
+		db_table = 'servers'
+		verbose_name = _("Сервер")
+		verbose_name_plural = _("Сервера")
