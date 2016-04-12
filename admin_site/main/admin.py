9040903e9aaa62806a76d6db8045325261e74f3b
Shadealex <5hnm@ukr.net>
2016-04-12 20:50:23 +0300

    add model statistics, chanche view name sever and verify

--- admin_site/main/admin.py
+++ admin_site/main/admin.py
@@ -1,10 +1,10 @@
 #-*-coding: utf-8 -*-
 from django.contrib import admin
-from main.models import Verify_type, Server
+from main.models import Verify_type, Server, Statistic
 
 # Register your models here.
 class Verify_typeAdmin(admin.ModelAdmin):
-	list_display = ['name', 'visibility']
+	list_display = ['name', 'visibility', 'comment']
 	list_filter = ['visibility']
 	search_fields = ['name']
 
@@ -16,3 +16,10 @@ class ServerAdmin(admin.ModelAdmin):
 	search_fields = ['name', 'ip']
 
 admin.site.register(Server, ServerAdmin)
+
+class StatisticAdmin(admin.ModelAdmin):
+	list_display = ['name', 'type_verify', 'date_verify', 'status_verify']
+	list_filter = ['name', 'type_verify', 'date_verify']
+	search_fields = ['name']
+
+admin.site.register(Statistic, StatisticAdmin)
--- admin_site/main/models.py
+++ admin_site/main/models.py
@@ -5,15 +5,14 @@ from django.conf import settings
 
 # Create your models here.
 
-# class Statistic(models.Model):
-	# pass
-
-	# def __unicode__(self):
-		# return self.name
 
 class Verify_type(models.Model):
-	name = models.CharField(verbose_name = _('Название'), max_length=255)
+	name = models.CharField(verbose_name = _('Название'), max_length=100)
 	visibility = models.BooleanField(verbose_name = _('Статус'), default=True, db_index=True)
+	comment = models.TextField(verbose_name = _('Комментарий'), blank=True)
+
+	def __str__(self):
+		return self.name
 
 	def __unicode__(self):
 		return self.name
@@ -25,13 +24,16 @@ class Verify_type(models.Model):
 		verbose_name_plural = _("Типы проверок")
 
 class Server(models.Model):
-	name = models.TextField(verbose_name = _('Имя сервера'))
+	name = models.TextField(verbose_name = _('Имя сервера'), max_length=100)
 	ip = models.GenericIPAddressField(protocol='both', verbose_name = _('IP сервера'))
 	date_add = models.DateField(auto_now_add=False, verbose_name = _('Дата добавления'))
 	date_edit = models.DateField(auto_now=False, verbose_name = _('Дата редактирования'))
 	verify = models.ManyToManyField(Verify_type, related_name='servers', verbose_name = _('Проверки'), blank=True)
 	on_off = models.BooleanField(verbose_name = _('Включение'))
 	comment = models.TextField(verbose_name = _('Комментарий'))
+
+	def __str__(self):
+		return self.name
 
 	def __unicode__(self):
 		return self.name
@@ -41,3 +43,21 @@ class Server(models.Model):
 		db_table = 'servers'
 		verbose_name = _("Сервер")
 		verbose_name_plural = _("Сервера")
+
+class Statistic(models.Model):
+	name = models.ForeignKey(Server, verbose_name=_('Имя сервера'))
+	type_verify = models.ForeignKey(Verify_type, verbose_name=_('Название теста'))
+	date_verify = models.DateTimeField(verbose_name = _('Дата проверки'), auto_now=False)
+	status_verify = models.CharField(verbose_name = _('Результат'), max_length=100)
+
+	def __str__(self):
+		return self.name
+
+	def __unicode__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['name']
+		db_table = 'Statistics'
+		verbose_name = _('Статистику')
+		verbose_name_plural = _('Статистика')
