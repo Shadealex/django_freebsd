45f7ec2fccd8acd14a30632471449470debfabd4
Alexey Maslak <5hnmalex@gmail.com>
2016-07-10 14:11:03 +0300

    Уменьшил длинну до 50 и 30 символов.

--- admin_site/main/models.py
+++ admin_site/main/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 
 class Verify_type(models.Model):
-	name = models.CharField(verbose_name = _('Название'), max_length=100, help_text =_('Название теста'))
+	name = models.CharField(verbose_name = _('Название'), max_length=50, help_text =_('Название теста'))
 	visibility = models.BooleanField(verbose_name = _('Статус'), default=True, db_index=True)
 	comment = models.TextField(verbose_name = _('Комментарий'), blank=True)
 
@@ -24,7 +24,7 @@ class Verify_type(models.Model):
 		verbose_name_plural = _("Типы проверок")
 
 class Server(models.Model):
-	name = models.TextField(verbose_name = _('Имя сервера'), max_length=100)
+	name = models.TextField(verbose_name = _('Имя сервера'), max_length=30)
 	ip = models.GenericIPAddressField(protocol='both', verbose_name = _('IP сервера'))
 	date_add = models.DateField(auto_now_add=False, verbose_name = _('Дата добавления'))
 	date_edit = models.DateField(auto_now=False, verbose_name = _('Дата редактирования'))
